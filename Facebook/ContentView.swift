8b29d403d015bca5ecbc2fae17f694383dd9061b
Silviu <capraru.silviu@hotmail.com>
2021-01-24 00:13:57 +0200

    Initial Commit

---
+++ Facebook/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Facebook
+//
+//  Created by Silviu on 24/01/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Facebook/FacebookApp.swift
@@ -0,0 +1,17 @@
+//
+//  FacebookApp.swift
+//  Facebook
+//
+//  Created by Silviu on 24/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct FacebookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
